f314c0f5977d901784c0d27d0640a6d6d3168a3b
yellow-sky <nikulin.e@gmail.com>
2015-01-31 16:27:42 +0300

    Tree panel update added. Tree is show real data now

--- src/tree_panel.py
+++ src/tree_panel.py
@@ -26,8 +26,13 @@ import os
 from PyQt4 import uic
 from PyQt4.QtGui import QDockWidget, QMainWindow
 from PyQt4.QtCore import Qt
+from qgis.core import QgsMessageLog
+from qgis.gui import QgsMessageBar
 
 from ngw_api.qgis.ngw_plugin_settings import NgwPluginSettings
+from ngw_api.qt.qt_ngw_resource_item import QNGWResourceItem
+from ngw_api.qt.qt_ngw_resource_model import QNGWResourcesModel
+from ngw_api.core.ngw_resource_factory import NGWResourceFactory
 
 __author__ = 'NextGIS'
 __date__ = 'January 2015'
@@ -60,4 +65,44 @@ class TreeControl(QMainWindow, FORM_CLASS):
 
         self.iface = iface
 
-        NgwPluginSettings.get_ngw_connection_names()
+        self.cmbConnection.currentIndexChanged[str].connect(self.reinit_tree)
+        self.update_conn_list()
+
+    def update_conn_list(self):
+        self.cmbConnection.clear()
+        self.cmbConnection.addItems(NgwPluginSettings.get_ngw_connection_names())
+
+        last_connection = NgwPluginSettings.get_selected_ngw_connection_name()
+        idx = self.cmbConnection.findText(last_connection)
+        if idx == -1 and self.cmbConnection.count() > 0:
+            self.cmbConnection.setCurrentIndex(0)
+        else:
+            self.cmbConnection.setCurrentIndex(idx)
+
+    def reinit_tree(self, name_of_conn):
+        #clear tree
+        self.trvResources.setModel(None)
+
+        if not name_of_conn:
+            return
+        conn_sett = NgwPluginSettings.get_ngw_connection(name_of_conn)
+
+        #setup ngw api
+        rsc_factory = NGWResourceFactory(conn_sett)
+
+        try:
+            root_rsc = rsc_factory.get_root_resource()
+        except Exception, e:
+            error_message = self.tr('Error on fetch resources: ') + e.message
+            self.iface.messageBar().pushMessage(self.tr('ERROR'),
+                                                error_message,
+                                                level=QgsMessageBar.CRITICAL)
+            QgsMessageLog.logMessage(error_message, level=QgsMessageLog.CRITICAL)
+            return
+
+        self._root_item = QNGWResourceItem(root_rsc, None)
+        self._resource_model = QNGWResourcesModel(self._root_item)
+        self.trvResources.setModel(self._resource_model)
+
+        NgwPluginSettings.set_selected_ngw_connection_name(name_of_conn)
+
