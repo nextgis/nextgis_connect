70e0cbf990c34fb6ff17849f063a61fa5e4b21ad
yellow-sky <nikulin.e@gmail.com>
2015-02-04 18:08:42 +0300

    Group create action was added

--- src/tree_panel.py
+++ src/tree_panel.py
@@ -30,6 +30,7 @@ from qgis.core import QgsMessageLog
 from qgis.gui import QgsMessageBar
 from ngw_api.core.ngw_error import NGWError
 from ngw_api.core.ngw_group_resource import NGWGroupResource
+from ngw_api.core.ngw_resource_creator import ResourceCreator
 from ngw_api.core.ngw_vector_layer import NGWVectorLayer
 from ngw_api.core.ngw_wfs_service import NGWWfsService
 
@@ -73,16 +74,20 @@ class TreeControl(QMainWindow, FORM_CLASS):
         self.actionCreateNewGroup.setIcon(QIcon(os.path.join(ICONS_PATH, 'mActionNewFolder.png')))
         self.actionCreateNewGroup.triggered.connect(self.create_group)
 
+        #update state
+        self.update_conn_list()
+        self.reinit_tree(self.cmbConnection.currentText())
+
         #signals
         self.cmbConnection.currentIndexChanged[str].connect(self.reinit_tree)
 
-        #update state
-        self.update_conn_list()
 
     def update_conn_list(self):
         self.cmbConnection.clear()
         self.cmbConnection.addItems(NgwPluginSettings.get_ngw_connection_names())
+        self.set_active_conn_from_sett()
 
+    def set_active_conn_from_sett(self):
         last_connection = NgwPluginSettings.get_selected_ngw_connection_name()
         idx = self.cmbConnection.findText(last_connection)
         if idx == -1 and self.cmbConnection.count() > 0:
@@ -91,14 +96,15 @@ class TreeControl(QMainWindow, FORM_CLASS):
             self.cmbConnection.setCurrentIndex(idx)
 
     def reinit_tree(self, name_of_conn):
-        #clear tree
+        # clear tree and states
         self.trvResources.setModel(None)
+        self.disable_tools()
 
         if not name_of_conn:
             return
         conn_sett = NgwPluginSettings.get_ngw_connection(name_of_conn)
 
-        #setup ngw api
+        # setup ngw api
         rsc_factory = NGWResourceFactory(conn_sett)
 
         try:
@@ -111,27 +117,33 @@ class TreeControl(QMainWindow, FORM_CLASS):
             QgsMessageLog.logMessage(error_message, level=QgsMessageLog.CRITICAL)
             return
 
-        #setup new qt model
+        # setup new qt model
         self._root_item = QNGWFakeRootItem(root_rsc, None)
         self._resource_model = QNGWResourcesModel(self._root_item)
         self.trvResources.setModel(self._resource_model)
-        #expand root item
+        # expand root item
         self.trvResources.setExpanded(self._resource_model.index(0, 0, QModelIndex()), True)
 
-        #reconnect signals
+        # reconnect signals
         self.trvResources.selectionModel().currentChanged.connect(self.active_item_chg)
 
-        #save last selected connection
+        # save last selected connection
         NgwPluginSettings.set_selected_ngw_connection_name(name_of_conn)
 
     def active_item_chg(self, selected, deselected):
         ngw_resource = selected.data(Qt.UserRole)
+        # TODO: NEED REFACTORING! Make isCompatible methods!
         # enable/dis geojson button
         self.actionAddAsGeoJSON.setEnabled(isinstance(ngw_resource, NGWVectorLayer))
         # enable/dis wfs button
         self.actionAddWFS.setEnabled(isinstance(ngw_resource, NGWWfsService))
         # enable/dis new group button
         self.actionCreateNewGroup.setEnabled(isinstance(ngw_resource, NGWGroupResource))
+
+    def disable_tools(self):
+        self.actionAddAsGeoJSON.setEnabled(False)
+        self.actionAddWFS.setEnabled(False)
+        self.actionCreateNewGroup.setEnabled(False)
 
     def add_json_layer(self):
         sel_index = self.trvResources.selectionModel().currentIndex()
@@ -150,7 +162,7 @@ class TreeControl(QMainWindow, FORM_CLASS):
 
     def add_wfs_layer(self):
         sel_index = self.trvResources.selectionModel().currentIndex()
-        print sel_index.isValid()
+
         if sel_index.isValid():
             ngw_resource = sel_index.data(Qt.UserRole)
 
@@ -165,7 +177,31 @@ class TreeControl(QMainWindow, FORM_CLASS):
 
 
     def create_group(self):
-        pass
+        sel_index = self.trvResources.selectionModel().currentIndex()
+
+        if sel_index.isValid():
+            ngw_resource = sel_index.data(Qt.UserRole)
+
+            new_group_name = 'New group'
+            existing_chd_names = [ch.common.display_name for ch in ngw_resource.get_children()]
+            if new_group_name in existing_chd_names:
+                id = 1
+                while(new_group_name+str(id) in existing_chd_names):
+                    id += 1
+                new_group_name += str(id)
+
+            try:
+                ResourceCreator.create_group(ngw_resource, new_group_name)
+            except NGWError as ex:
+                error_mes = ex.message or ''
+                self.iface.messageBar().pushMessage(self.tr('Error'),
+                                                error_mes,
+                                                level=QgsMessageBar.CRITICAL)
+                QgsMessageLog.logMessage(error_mes, level=QgsMessageLog.CRITICAL)
+
+            self.reinit_tree(self.cmbConnection.currentText())
+            # TODO: need more flex update
+
 
 
 
