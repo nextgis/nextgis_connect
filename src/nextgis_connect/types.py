67cc3371c30e8608a9aecdbaa69eab73f9db44eb
Ivan Barsukov <i.barsukov@protonmail.com>
2026-02-14 14:12:48 +0300

    Add type definitions and UnsetType class for clarity

--- src/nextgis_connect/types.py
+++ src/nextgis_connect/types.py
@@ -1,3 +1,5 @@
+from typing import TypeVar, Union
+
 from nextgis_connect.compat import QgsFeatureId
 
 FeatureId = QgsFeatureId
@@ -7,3 +9,26 @@ AttachmentId = int
 NgwFeatureId = int
 NgwFieldId = int
 NgwAttachmentId = int
+
+VersionId = int
+
+FileObjectId = int
+
+WktString = str
+Wkb64String = str
+
+
+class UnsetType:
+    """Represent an unset value."""
+
+    def __repr__(self) -> str:
+        return "<UNSET>"
+
+    def __bool__(self):
+        return False
+
+
+Unset = UnsetType()
+
+T = TypeVar("T")
+Unsettable = Union[T, UnsetType]
